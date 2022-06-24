b4b15596db2df690522d573d0267591d8cd3ad2a
mrmosssir <mrmosssir@gmail.com>
2022-06-24 13:40:15 +0800

    feature - 2022-06-23 - 調整設定檔

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,74 @@
-import { defineConfig } from 'vite'
-import EnvironmentPlugin from 'vite-plugin-environment';
+import { defineConfig } from "vite"
+// import EnvironmentPlugin from "vite-plugin-environment";
 
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
-import path from 'path'
+import path from "path";
+import "dotenv/config";
+
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [react()],
+    base: "/music-player/",
+    define: {
+      "process.env": {
+        "AUTH_BASE_URL": process.env.AUTH_BASE_URL,
+        "API_BASE_URL": process.env.API_BASE_URL,
+        "CLIENT_ID": process.env.CLIENT_ID,
+        "CLIENT_SECRET": process.env.CLIENT_SECRET,
+        "SITE_DOMAIN": process.env.SITE_DOMAIN
+      }
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src")
+      }
+    },
+  })
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), EnvironmentPlugin('all', { prefix: '' })],
-  base: "/music-player/",
-  define: {
-    'process.env': {}
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-})
+// export default defineConfig({
+//   plugins: [react(), EnvironmentPlugin({
+//     "AUTH_BASE_URL": process.env.AUTH_BASE_URL,
+//     "API_BASE_URL": process.env.API_BASE_URL,
+//     "CLIENT_ID": process.env.CLIENT_ID,
+//     "CLIENT_SECRET": process.env.CLIENT_SECRET,
+//     "SITE_DOMAIN": process.env.SITE_DOMAIN
+//   })],
+//   base: "/music-player/",
+//   define: {
+//     'process.env': {}
+//   },
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src')
+//     }
+//   },
+// })
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd())
+
+//   const processEnvValues = {
+//     'process.env': Object.entries(env).reduce(
+//       (prev, [key, val]) => {
+//         return {
+//           ...prev,
+//           [key]: val,
+//         }
+//       },
+//       {},
+//     )
+//   }
+//   return {
+//     plugins: [react()],
+//     base: "/music-player/",
+//     define: processEnvValues,
+//     resolve: {
+//       alias: {
+//         '@': path.resolve(__dirname, './src')
+//       }
+//     },
+//   }
+// })
